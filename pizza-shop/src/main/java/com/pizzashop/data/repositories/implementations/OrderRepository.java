f710007ad30af37666cc7dea5654cb6c5427c6dd
VChouliak <chouliak@hotmail.com>
2020-08-14 17:38:02 +0200

    Create repository

--- pizza-shop/src/main/java/com/pizzashop/data/repositories/implementations/OrderRepository.java
+++ pizza-shop/src/main/java/com/pizzashop/data/repositories/implementations/OrderRepository.java
@@ -1,6 +1,7 @@
 package com.pizzashop.data.repositories.implementations;
 
 import com.pizzashop.data.models.Order;
+import com.pizzashop.data.repositories.IOrderRepository;
 
-public class OrderRepository extends BaseRepository<Order> implements IOrderRepository{
+public class OrderRepository extends BaseRepository<Order> implements IOrderRepository {
 }
--- pizza-shop/src/main/java/com/pizzashop/data/repositories/implementations/UserRepository.java
+++ pizza-shop/src/main/java/com/pizzashop/data/repositories/implementations/UserRepository.java
@@ -1,6 +1,7 @@
 package com.pizzashop.data.repositories.implementations;
 
 import com.pizzashop.data.models.User;
+import com.pizzashop.data.repositories.IUserRepository;
 
-public class UserRepository extends BaseRepository<User> implements IUserRepository{
+public class UserRepository extends BaseRepository<User> implements IUserRepository {
 }
