ffcf3ae5520b9fba91faa4ea8b855b47772e2269
VChouliak <chouliak@hotmail.com>
2020-08-15 11:02:08 +0200

    Implement service

--- pizza-shop/src/main/java/com/pizzashop/data/services/implementations/OrderDataService.java
+++ pizza-shop/src/main/java/com/pizzashop/data/services/implementations/OrderDataService.java
@@ -1,4 +1,43 @@
 package com.pizzashop.data.services.implementations;
 
-public class OrderDataService {
+import com.pizzashop.data.models.Order;
+import com.pizzashop.data.repositories.IOrderRepository;
+import com.pizzashop.data.services.IOrderDataService;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OrderDataService implements IOrderDataService {
+    private IOrderRepository repository;
+
+    public OrderDataService(IOrderRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Order> getAllOrders() {
+        List<Order> orders = repository.getAll();
+        return orders.isEmpty() ? null : orders;
+    }
+
+    @Override
+    public Order getOrderById(int id) {
+        Optional<Order> order = repository.findBy("id", id).stream().findFirst();
+        return order.isPresent() ? order.get() : null;
+    }
+
+    @Override
+    public boolean createOrder(Order order) {
+        return repository.create(order);
+    }
+
+    @Override
+    public boolean deleteOrder(Order order) {
+        return deleteOrder(order);
+    }
+
+    @Override
+    public boolean updateOrder(Order order) {
+        return repository.update(order);
+    }
 }
