4256f93463ebf1562b9d158ef8fe1db55afb5677
VChouliak <chouliak@hotmail.com>
2020-08-16 23:19:03 +0200

    Add Secured annotation to getAllOrders

--- pizza-shop/src/main/java/com/pizzashop/controllers/OrderController.java
+++ pizza-shop/src/main/java/com/pizzashop/controllers/OrderController.java
@@ -4,16 +4,13 @@ import com.pizzashop.data.models.Order;
 import com.pizzashop.data.repositories.implementations.OrderRepository;
 import com.pizzashop.data.services.IOrderDataService;
 import com.pizzashop.data.services.implementations.OrderDataService;
+import com.pizzashop.security.interfaces.Secured;
+import com.pizzashop.security.modells.Role;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Optional;
 
 @Path("/order")
 @Consumes({MediaType.APPLICATION_JSON})
@@ -26,10 +23,11 @@ public class OrderController {
     }
 
     @GET
+    @Secured({Role.USER})
     @Path("/get/all")
-    public Response getAllOrders(){
+    public Response getAllOrders() {
         List<Order> orders = dataService.getAllOrders();
-        if(orders.isEmpty()){
+        if (orders.isEmpty()) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
         return Response.ok(orders).build();
@@ -37,9 +35,9 @@ public class OrderController {
 
     @GET
     @Path("/get/{id}")
-    public Response getOrderById(@PathParam("id") int id){
+    public Response getOrderById(@PathParam("id") int id) {
         Order order = dataService.getOrderById(id);
-        if(order == null){
+        if (order == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
         return Response.ok(order).build();
@@ -47,12 +45,11 @@ public class OrderController {
 
     @GET
     @Path("/get/user")
-    public Response getOrdersByUser(@QueryParam("id") int id){
+    public Response getOrdersByUser(@QueryParam("id") int id) {
         List<Order> orders = dataService.getOrdersByUser(id);
-        if(orders.isEmpty()){
+        if (orders.isEmpty()) {
             return Response.status(Response.Status.NOT_FOUND).build();
-        }
-        else if(!orders.isEmpty()){
+        } else if (!orders.isEmpty()) {
             return Response.ok(orders).build();
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
@@ -60,8 +57,8 @@ public class OrderController {
 
     @POST
     @Path("/create")
-    public Response createOrder(Order order){
-        if(dataService.createOrder(order)){
+    public Response createOrder(Order order) {
+        if (dataService.createOrder(order)) {
             return Response.status(Response.Status.CREATED).build();
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
@@ -69,11 +66,11 @@ public class OrderController {
 
     @PUT
     @Path("/update")
-    public Response updateOrder(Order order){
-        if(dataService.getOrderById(order.getId()) == null){
+    public Response updateOrder(Order order) {
+        if (dataService.getOrderById(order.getId()) == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
-        if(dataService.updateOrder(order)){
+        if (dataService.updateOrder(order)) {
             return Response.status(Response.Status.NO_CONTENT).build();
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
@@ -81,11 +78,11 @@ public class OrderController {
 
     @DELETE
     @Path("/delete")
-    public Response deleteOrder(Order order){
-        if(dataService.getOrderById(order.getId()) == null){
+    public Response deleteOrder(Order order) {
+        if (dataService.getOrderById(order.getId()) == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
-        if(dataService.deleteOrder(order)){
+        if (dataService.deleteOrder(order)) {
             return Response.status(Response.Status.NO_CONTENT).build();
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
@@ -93,11 +90,11 @@ public class OrderController {
 
     @DELETE
     @Path("/delete/{id}")
-    public Response deleteOrderById(@PathParam("id") int id){
-        if(dataService.getOrderById(id) == null){
+    public Response deleteOrderById(@PathParam("id") int id) {
+        if (dataService.getOrderById(id) == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
-        if(dataService.deleteOrderById(id)){
+        if (dataService.deleteOrderById(id)) {
             return Response.status(Response.Status.NO_CONTENT).build();
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
