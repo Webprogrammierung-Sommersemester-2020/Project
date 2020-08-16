9ce3a69bbd5e395ee9576b4bc771e41ca18b1411
VChouliak <chouliak@hotmail.com>
2020-08-16 23:19:29 +0200

    Register Classes

--- pizza-shop/src/main/java/com/pizzashop/controllers/Configuration.java
+++ pizza-shop/src/main/java/com/pizzashop/controllers/Configuration.java
@@ -1,8 +1,21 @@
 package com.pizzashop.controllers;
 
+
+import com.pizzashop.security.AuthorizationFilter;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/api")
 public class Configuration extends Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<>();
+        classes.add(AuthController.class);
+        classes.add(OrderController.class);
+        classes.add(AuthorizationFilter.class);
+        return classes;
+    }
 }
