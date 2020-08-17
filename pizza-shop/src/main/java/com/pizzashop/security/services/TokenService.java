74e4fdcbef8a406c51fe62ddef01e07486975efd
VChouliak <chouliak@hotmail.com>
2020-08-17 15:04:17 +0200

    Implement methods

--- pizza-shop/src/main/java/com/pizzashop/security/services/TokenService.java
+++ pizza-shop/src/main/java/com/pizzashop/security/services/TokenService.java
@@ -5,12 +5,12 @@ import com.pizzashop.data.repositories.implementations.UserRepository;
 import com.pizzashop.data.services.IUserDataService;
 import com.pizzashop.data.services.implementations.UserDataService;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.core.Context;
 import javax.xml.bind.DatatypeConverter;
 import java.nio.charset.StandardCharsets;
@@ -23,54 +23,78 @@ import java.util.StringTokenizer;
 
 public class TokenService {
 
-    private final IUserDataService userDataService;
+    private static HttpSession session = null;
+    private static IUserDataService userDataService = new UserDataService(new UserRepository());
 
-    public TokenService() {
-        this.userDataService = new UserDataService(new UserRepository());
+    private static String getCredentialsStringFrom(String authorization) {
+
+        String base64Credentials = authorization.substring("Basic".length()).trim();
+        byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
+
+        return new String(credDecoded, StandardCharsets.UTF_8);
     }
 
-    public String create(@Context HttpServletRequest request) {
+    private static boolean isBasic(String authorization) {
+        return authorization != null && authorization.toLowerCase().startsWith("basic");
+    }
+
+    public static String create(@Context HttpServletRequest request) {
         User user;
         StringTokenizer tokenizer = null;
-        final String authorization = request.getHeader("Authorization");
-        if (authorization != null && authorization.toLowerCase().startsWith("basic")) {
 
-            String base64Credentials = authorization.substring("Basic".length()).trim();
-            byte[] credDecoded = Base64.getDecoder().decode(base64Credentials);
-            String credentials = new String(credDecoded, StandardCharsets.UTF_8);
+        final String authorization = request.getHeader("Authorization");
+
+        if (isBasic(authorization)) {
+
+            String credentials = getCredentialsStringFrom(authorization);
             tokenizer = new StringTokenizer(credentials, ":");
 
-        }
-        final String username = tokenizer.nextToken();
-        final String password = tokenizer.nextToken();
+            final String username = tokenizer.nextToken();
+            final String password = tokenizer.nextToken();
 
-        if (username != null) {
-            user = userDataService.getAllUsers().stream().filter(u -> u.getUserName().equals(username)).findFirst().get();
-            if (password.equals(user.getPassword())) {
-                Key signingKey = new SecretKeySpec(
-                        DatatypeConverter.parseBase64Binary(password), SignatureAlgorithm.HS512.getJcaName());
-                return Jwts.builder()
-                        .setSubject(username)
-                        .claim("role", user.getRole())
-                        .signWith(SignatureAlgorithm.HS512, signingKey)
-                        .setExpiration(Date.from(Instant.now().plus(30, ChronoUnit.MINUTES)))
-                        .compact();
+            if (username != null) {
+                user = userDataService.getUserByUserName(username);
+                if (password.equals(user.getPassword())) {
+                    Key signingKey = new SecretKeySpec(
+                            DatatypeConverter.parseBase64Binary(password), SignatureAlgorithm.HS512.getJcaName());
+
+                    session = request.getSession();
+                    session.setAttribute("username", user.getUserName());
+
+                    return Jwts.builder()
+                            .setSubject(username)
+                            .claim("role", user.getRole())
+                            .signWith(SignatureAlgorithm.HS512, signingKey)
+                            .setExpiration(Date.from(Instant.now().plus(30, ChronoUnit.MINUTES)))
+                            .compact();
+                }
             }
         }
         return null;
     }
 
-    public static String validateTokenAndReturnRole(String token, String password) {
-        Key signingKey = new SecretKeySpec(
-                DatatypeConverter.parseBase64Binary(password), SignatureAlgorithm.HS512.getJcaName());
-        Claims claims = Jwts.parser().setSigningKey(signingKey).parseClaimsJws(token).getBody();
-        String role = null;
-        role = claims.get("role").toString();
-        Jwts.parser()
-                .requireSubject(claims.getSubject())
-                .setSigningKey(signingKey)
-                .parseClaimsJws(token);
+    public static String validateTokenAndReturnRole(String token) {
+        User user;
+        String role = "";
+        if ((session != null)) {
+            if (session.getAttribute("username") != null) {
+                String username = (String) session.getAttribute("username");
+                if (!username.isBlank()) {
+                    user = userDataService.getUserByUserName(username);
+                    Key signingKey = new SecretKeySpec(
+                            DatatypeConverter.parseBase64Binary(user.getPassword()), SignatureAlgorithm.HS512.getJcaName());
+                    Claims claims = Jwts.parser().setSigningKey(signingKey).parseClaimsJws(token).getBody();
 
+                    role = claims.get("role").toString();
+                    Jwts.parser()
+                            .requireSubject(claims.getSubject())
+                            .setSigningKey(signingKey)
+                            .parseClaimsJws(token);
+                }
+            }
+
+        }
         return role;
     }
+
 }
