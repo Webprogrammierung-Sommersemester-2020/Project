a923c8c21673938413a8b1d466687da75bd5a5eb
VChouliak <chouliak@hotmail.com>
2020-08-17 14:55:31 +0200

    Delete id from Address

--- pizza-shop/src/main/java/com/pizzashop/data/models/Address.java
+++ pizza-shop/src/main/java/com/pizzashop/data/models/Address.java
@@ -1,40 +1,31 @@
 package com.pizzashop.data.models;
 
 public class Address {
-    private int id;
     private String street;
     private int number;
-    private String sufix;
+    private String suffix;
     private String zipCode;
     private String city;
 
     public Address() {
     }
 
-    public Address(String street, int number, String sufix, String zipCode, String city) {
+    public Address(String street, int number, String suffix, String zipCode, String city) {
         this.street = street;
         this.number = number;
-        this.sufix = sufix;
+        this.suffix = suffix;
         this.zipCode = zipCode;
         this.city = city;
     }
 
-    public Address(int id, String street, int number, String sufix, String zipCode, String city) {
-        this.id = id;
+    public Address(int id, String street, int number, String suffix, String zipCode, String city) {
         this.street = street;
         this.number = number;
-        this.sufix = sufix;
+        this.suffix = suffix;
         this.zipCode = zipCode;
         this.city = city;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getStreet() {
         return street;
@@ -52,12 +43,12 @@ public class Address {
         this.number = number;
     }
 
-    public String getSufix() {
-        return sufix;
+    public String getSuffix() {
+        return suffix;
     }
 
-    public void setSufix(String sufix) {
-        this.sufix = sufix;
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
     }
 
     public String getZipCode() {
@@ -76,55 +67,5 @@ public class Address {
         this.city = city;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((city == null) ? 0 : city.hashCode());
-        result = prime * result + id;
-        result = prime * result + number;
-        result = prime * result + ((street == null) ? 0 : street.hashCode());
-        result = prime * result + ((sufix == null) ? 0 : sufix.hashCode());
-        result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Address other = (Address) obj;
-        if (city == null) {
-            if (other.city != null)
-                return false;
-        } else if (!city.equals(other.city))
-            return false;
-        if (id != other.id)
-            return false;
-        if (number != other.number)
-            return false;
-        if (street == null) {
-            if (other.street != null)
-                return false;
-        } else if (!street.equals(other.street))
-            return false;
-        if (sufix == null) {
-            if (other.sufix != null)
-                return false;
-        } else if (!sufix.equals(other.sufix))
-            return false;
-        if (zipCode == null) {
-            if (other.zipCode != null)
-                return false;
-        } else if (!zipCode.equals(other.zipCode))
-            return false;
-        return true;
-    }
-
-    
 
 }
