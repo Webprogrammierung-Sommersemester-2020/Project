37d5999df016be3efb91d1ab4c54bcfc76e16bfe
VChouliak <chouliak@hotmail.com>
2020-08-25 17:24:07 +0200

    Implement methods to add and delete pizzaElement to/from page and list

--- pizza-shop/src/main/webapp/js/services/pizzaservice.js
+++ pizza-shop/src/main/webapp/js/services/pizzaservice.js
@@ -9,4 +9,85 @@ export default class PizzaService {
                 window.location.href = "details.html";
             }).catch(error => console.log(error))
     }
+
+    static addPizzaElementToPage(pizza, btnName) {
+        let name = pizza.name;
+        let ingredients = "";
+        let price = pizza.price;
+        let pictureName = ""
+        let sizes = "";
+
+        if(pizza.name.toLowerCase() === "Generierte pizza".toLowerCase()){
+            pictureName = "Custom";
+        }
+        else {
+            pictureName = pizza.name;
+        }
+        pizza.ingredients.forEach((ingredient) => {
+            ingredients += ingredient.name + ": " + ingredient.price + " &euro;<br>";
+        });
+
+
+        let orderContentContainer = document.querySelector(".order-container");
+
+        let rowDivContainer = document.createElement("div");
+        rowDivContainer.className = "row";
+
+        let orderDivContainer = document.createElement("div");
+        orderDivContainer.className = "order";
+
+        let imgContainer = document.createElement("img");
+        imgContainer.src = "img/"+pictureName+".jpg";
+
+        let figureContainer = document.createElement("figure");
+        figureContainer.className = "order__shape";
+
+        let h3Tag = document.createElement("h3");
+        h3Tag.innerHTML = name;
+
+        let paragraphTag = document.createElement("p");
+        paragraphTag.innerHTML = ingredients+ "<h4>Gesamtpreis: " + price + " &euro; <a href='#' id='eventFor"+name+"' >"+btnName+"</a></h4><br>";
+
+        let orderTextContainer = document.createElement("div");
+        orderTextContainer.className = "order__text";
+
+
+        figureContainer.appendChild(imgContainer);
+        orderTextContainer.append(h3Tag, paragraphTag)
+        orderDivContainer.append(figureContainer, orderTextContainer);
+        rowDivContainer.appendChild(orderDivContainer);
+        orderContentContainer.appendChild(rowDivContainer);
+
+    }
+
+    static addPizzaToList(pizza) {
+        let pizzas = JSON.parse(window.sessionStorage.getItem("pizzas"));
+        let localPizza = {
+            name: pizza.name,
+            ingredients: pizza.ingredients.map((ingredient)=>{
+                return ingredient;
+            }),
+            sizes: pizza.sizes.map((size)=>{
+                return size;
+            }),
+            price: pizza.price
+        }
+
+        if (!pizzas){
+            pizzas = new Array();
+        }
+
+        pizzas.push(localPizza);
+        const pizzasJson = JSON.stringify(pizzas)
+        window.sessionStorage.setItem("pizzas",pizzasJson);
+        console.log(pizzas);
+    }
+
+    static deletePizzaFromList(pizzaName){
+        const pizzaEntry = pizzas.find(pizza=>pizza.name === pizzaName);
+        const index = pizzas.indexOf(pizzaEntry);
+        pizzas.splice(index,1);
+        window.sessionStorage.setItem("pizzas",JSON.stringify(pizzas));
+        window.location.reload();
+    }
 }
