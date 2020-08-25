dd5aba52c9561056a111057856ce64a7d656d13b
VChouliak <chouliak@hotmail.com>
2020-08-25 17:25:20 +0200

    Implement methods

--- pizza-shop/src/main/webapp/js/homepage.js
+++ pizza-shop/src/main/webapp/js/homepage.js
@@ -1,17 +1,30 @@
 import PizzaService from "./services/pizzaservice.js";
 import HttpService from "./services/httpservice.js";
 
-
+let pizzas;
+let offerContainer = document.querySelector(".offer");
 window.onload = () => {
-    HttpService.doRequest('/pizza-shop/api/pizza/get/all', 'GET')
-        .then((jsonString) => {
-            const pizzas = JSON.parse(jsonString);
-        })
-        .catch(err => console.log(err));
+ HttpService.doRequest("/pizza-shop/api/pizza/get/all", "GET")
+     .then(jsonString=>{
+         pizzas = JSON.parse(jsonString);
+         pizzas.forEach((pizza)=>{
+             PizzaService.addPizzaElementToPage(pizza, "Zum Warenkorb Hinzu&uuml;gen");
+             let addToCardButton = document.getElementById("eventFor"+pizza.name);
+             addToCardButton.onclick = () =>{
+                 PizzaService.addPizzaToList(pizza);
+             }
+         });
+     })
+     .catch(error=>console.log(error))
 }
 
+offerContainer.onclick = () =>{
+    redirectToPizzaGenerator();
+}
 
-
+function redirectToPizzaGenerator(){
+    window.location.href="pizzabuilder.html";
+}
 
 
 
