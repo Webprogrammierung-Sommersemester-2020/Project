87bec9f2bfc47b3dd6fac0a15255d3f292d64cc4
VChouliak <chouliak@hotmail.com>
2020-08-26 18:45:20 +0200

    Create and implement authservice.js

---
+++ pizza-shop/src/main/webapp/js/services/authservice.js
@@ -0,0 +1,36 @@
+import HttpService from "./httpservice.js";
+
+export default class AuthService {
+    token = "";
+
+    static sendAuthRequest = (username, password) => {
+        const credentials = "Basic " + btoa(username + ":" + password)
+        HttpService.doRequest("/pizza-shop/api/auth/token", "GET", {"Authorization": credentials})
+            .then(
+                (response) => {
+                    this.token = response;
+                    if (this.token) {
+                        window.sessionStorage.setItem("token", this.token);
+                    }
+                }
+            )
+            .catch(error => console.log(error))
+    }
+
+    static isAuthorized() {
+        this.token = window.sessionStorage.getItem("token");
+        if (!this.token) {
+            return false;
+        }
+        return true;
+    }
+
+    static setAuthTokenToSessionAs(itemName) {
+        if (this.token) {
+            const authToken = "Baerer " + this.token;
+            window.sessionStorage.setItem(itemName, authToken);
+            return true;
+        }
+        return false;
+    }
+}
