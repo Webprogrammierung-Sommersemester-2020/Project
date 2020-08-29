245d65864d412d6cee5c86ad29910c96d66218ec
VChouliak <chouliak@hotmail.com>
2020-08-29 10:47:18 +0200

    Update event for addToCartButton

--- pizza-shop/src/main/webapp/js/homepage.js
+++ pizza-shop/src/main/webapp/js/homepage.js
@@ -5,26 +5,34 @@ let offerContainer = document.getElementById("offer");
 let cartButton = document.getElementById("cart");
 
 window.onload = () => {
- PizzaService.getAllPizzas()
-     .then(jsonString=>{
-         pizzas = JSON.parse(jsonString);
-         pizzas.forEach((pizza)=>{
-             PizzaService.addPizzaElementToPage(pizza, "Zum Warenkorb Hinzuf&uuml;gen");
-             let addToCardButton = document.getElementById("eventFor"+pizza.name);
-             addToCardButton.onclick = () =>{
-                 PizzaService.addPizzaToList(pizza);
-             }
-         });
-     })
-     .catch(error=>console.log(error))
+    PizzaService.getAllPizzas()
+        .then(jsonString => {
+            pizzas = JSON.parse(jsonString);
+            pizzas.forEach((pizza) => {
+                PizzaService.addPizzaElementToPage(pizza, "Zum Warenkorb Hinzuf&uuml;gen");
+                let addToCardButton = document.getElementById("eventFor" + pizza.name);
+                addToCardButton.onclick = () => {
+                    PizzaService.addPizzaToList(pizza);
+                    addToCardButton.innerHTML = "Eingef&uuml;gt"
+                    addToCardButton.setAttribute("style",
+                        "color:green; " +
+                        "border:none;"+
+                        "cursor: not-allowed;" +
+                        "pointer-events:none;" +
+                        "opacity:0.5; " +
+                        "text-decoration:none;");
+                }
+            });
+        })
+        .catch(error => console.log(error))
 }
 
-offerContainer.onclick = () =>{
-    window.location.href="pizzabuilder.html";
+offerContainer.onclick = () => {
+    window.location.href = "pizzabuilder.html";
 }
 
 cartButton.onclick = () => {
-    window.location.href="cart.html";
+    window.location.href = "cart.html";
 }
 
 
